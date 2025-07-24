d8af3694a417e20a2086140b6cb86fffb1267ec5
Transfomer Wakanda <w4k4nda@kali.kalilinux>
2025-07-24 15:34:16 +0300

    last commit for deployment

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
     <footer className="bg-gray-900 text-white py-12">
       <div className="container mx-auto px-6">
         <div className="text-center">
-          <h3 className="text-2xl font-bold mb-4">Wangui Margarey</h3>
-          <p className="text-gray-400 mb-6">Backend Developer|Enthusiast  Data Analyst</p>
+          <h3 className="text-2xl font-bold mb-4">Wangui Margaret</h3>
+          <p className="text-gray-400 mb-6">Backend Developer|Enthusiast Data Analyst</p>
           
           <div className="flex justify-center space-x-6 mb-8">
             <a 
